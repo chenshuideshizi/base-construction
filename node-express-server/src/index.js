b5f4169c1ef89e128067e313f8e0f7d5f01e1064
fanwenxiang <fanwenxiang@aiforward.com>
2021-12-21 14:47:49 +0800

    fix:添加jsonp

--- node-express-server/src/index.js
+++ node-express-server/src/index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
 
-const port = 3000
-
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+// jsonp
+app.get('/jsonp', (req, res) => {
+    console.log(req)
+    const { callback, name } = req.query
+    const data = {
+        code: 200,
+        data: {
+            name: name,
+            extract: 'Hello World'
+        }
+    }
 
+    // callback 的参数需要 使用 JSON.stringify
+    res.send(`${callback}(${JSON.stringify(data)})`)
+})
+
+const port = 3000
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
